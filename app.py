8098d87262a490f052365d696079bab8540492e5
Maciej Hoffmann <133579377+hfasthfasthfast@users.noreply.github.com>
2023-05-15 12:05:18 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+"""
+tekst
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """ zwraca podany ponizej tekst """
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
